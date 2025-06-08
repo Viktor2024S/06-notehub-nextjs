bae3224b3cb52936d741a8aedf8b3a7ac33ac133
Viktor Momotenko <viktor.momotenko@gmail.com>
2025-06-08 14:18:22 +0100

    types: add notes type

---
+++ src/types/note.ts
@@ -0,0 +1,12 @@
+export type Tag = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+
+export interface Note {
+  id: string;
+  title: string;
+  content: string;
+  tag: Tag;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type NoteData = Pick<Note, "title" | "content" | "tag">;
