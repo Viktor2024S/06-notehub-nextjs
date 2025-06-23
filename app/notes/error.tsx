f3e5505ee42c9696a741beade778a5beb9dde6f2
Viktor Momotenko <viktor.momotenko@gmail.com>
2025-06-23 21:25:28 +0100

    fixed error.tsx

--- app/notes/error.tsx
+++ app/notes/error.tsx
@@ -1,15 +1,9 @@
-// "use client";
-
-// export default function Error({
-//   error,
-// }: {
-//   error: Error & { digest?: string };
-// }) {
-//   return <p>Could not fetch the list of notes. {error.message}</p>;
-// }
-
 "use client";
 
-export default function Error() {
-  return <p>Error component works.</p>;
+export default function Error({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  return <p>Could not fetch the list of notes. {error.message}</p>;
 }
